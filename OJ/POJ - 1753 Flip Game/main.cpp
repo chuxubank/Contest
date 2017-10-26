4ce66d20bbd79414d1937fe373b371fb7a9181fb
Kirito <chuxubank@qq.com>
2017-10-26 14:09:02 +0800

    ACM Knowledge + DFS patch

--- OJ/POJ - 1753 Flip Game/main.cpp
+++ OJ/POJ - 1753 Flip Game/main.cpp
@@ -2,69 +2,58 @@
 #include <climits>
 using namespace std;
 int a[6][6];
-int minn=INT_MAX;
-int judge()
-{
-    if(a[1][1]==-1)
-    {
-        for(int i=1;i<=4;i++)
-            for(int j=1;j<=4;j++)
-                if(a[i][j]!=-1)
-                    return 0;
-    }
-    else
-    {
-        for(int i=1;i<=4;i++)
-            for(int j=1;j<=4;j++)
-                if(a[i][j]!=1)
-                    return 0;
-    }
-    return 1;
+int minn = INT_MAX;
+bool judge() {
+	for (int i = 1; i <= 4; i++) {
+		for (int j = 1; j <= 4; j++) {
+			if (a[1][1] != a[i][j]) {
+				return false;
+			}
+		}
+	}
+	return true;
 }
 
-void flip(int x,int y)
+void flip(int x, int y)
 {
-    a[x][y]=-a[x][y];
-    a[x-1][y]=-a[x-1][y];
-    a[x][y-1]=-a[x][y-1];
-    a[x+1][y]=-a[x+1][y];
-    a[x][y+1]=-a[x][y+1];
-//    for(int i=1;i<=4;i++)
-//    {
-//        for(int j=1;j<=4;j++)
-//            cout<<a[i][j];
-//        cout<<endl;
-//    }
+	a[x][y] = -a[x][y];
+	a[x - 1][y] = -a[x - 1][y];
+	a[x][y - 1] = -a[x][y - 1];
+	a[x + 1][y] = -a[x + 1][y];
+	a[x][y + 1] = -a[x][y + 1];
 }
 
-void dfs(int cur,int step)
+void dfs(int cur, int count)
 {
-    if(cur==17)
-    {
-        if(judge() && minn>step)
-            minn=step;
-        return;
-    }
-    flip(cur/4+1,cur%4+1);
-    dfs(cur+1,step+1);
-    flip(cur/4+1,cur%4+1);
-    dfs(cur+1,step);
+	if (cur==17)
+	{
+		if (judge() && minn>count)
+			minn = count;
+		return;
+	}
+	flip((cur - 1) / 4 + 1, (cur - 1) % 4 + 1);
+	dfs(cur + 1, count + 1);
+	flip((cur - 1) / 4 + 1, (cur - 1) % 4 + 1);
+	dfs(cur + 1, count);
 }
 int main()
 {
-    char b[6][6];
-    for(int i=1;i<=4;i++)
-    {
-        for(int j=1;j<=4;j++)
-        {
-            cin>>b[i][j];
-            if(b[i][j]=='b')
-                a[i][j]=1;
-            if(b[i][j]=='w')
-                a[i][j]=-1;
-        }
-        getchar();
-    }
-    dfs(0,0);
-    cout<<minn<<endl;
+	char b[6][6];
+	for (int i = 1; i <= 4; i++)
+	{
+		for (int j = 1; j <= 4; j++)
+		{
+			cin >> b[i][j];
+			if (b[i][j] == 'b')
+				a[i][j] = 1;
+			if (b[i][j] == 'w')
+				a[i][j] = -1;
+		}
+		getchar();
+	}
+	dfs(0, 0);
+	if (minn == INT_MAX)
+		cout << "Impossible" << endl;
+	else
+		cout << minn << endl;
 }
