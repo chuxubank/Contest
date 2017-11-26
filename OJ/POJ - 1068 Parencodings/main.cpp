60025ec5b65261c281f6fddd661d0944cabe51ac
Kirito <chuxubank@qq.com>
2017-11-26 09:33:24 +0800

    Just simulation.

--- OJ/POJ - 1068 Parencodings/main.cpp
+++ OJ/POJ - 1068 Parencodings/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstdio>
+#include <string>
 using namespace std;
 
 const int maxn = 1e9+7;
@@ -7,14 +7,43 @@ const int maxn = 1e9+7;
 int main()
 {
     string p;
-	int t,n,a[25];
+	int t,n,a[25]={0};
 	cin>>t;
 	while(t--)
     {
+        string s;
         cin>>n;
-        for(int i=0;i<n;i++)
+        for(int i=1;i<=n;i++)
             cin>>a[i];
-
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<a[i]-a[i-1];j++)
+                s.push_back('(');
+            s.push_back(')');
+        }
+        int j,pair,ans;
+        for(int i=0;i<s.length();i++)
+        {
+            ans=0;
+            pair=1;
+            if(s[i]==')')
+            {
+                j=i-1;
+                while(pair && j>=0)
+                {
+                    if(s[j]=='(')
+                    {
+                        pair--;
+                        ans++;
+                    }
+                    else
+                        pair++;
+                    j--;
+                }
+                cout<<ans<<" ";
+            }
+        }
+        cout<<endl;
     }
     return 0;
 }
